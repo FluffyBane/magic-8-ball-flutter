4d1bdcf3fc9d9b39af7eca6ad068bd1ac7bc7626
Stephen <stephen@disa.im>
2020-02-04 23:09:17 +0000

    Create Ball widget and display the magic 8 ball image

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,31 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.blue[500],
       appBar: AppBar(
         title: Text('Ask Me Anything'),
-        backgroundColor: Colors.blue[900],
+        backgroundColor: Colors.blue[700],
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(36.0),
+        child: Image.asset(
+          'images/ball1.png',
+        ),
+      ),
     );
   }
 }
